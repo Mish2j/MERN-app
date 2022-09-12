5a1220e9d69762f65f27184892bea91b326c3248
Mish2j <mishelghukasyan@gmail.com>
2022-09-11 20:30:03 -0700

    Added new components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useState, useCallback } from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
+import UserPlaces from "./places/pages/UserPlaces";
+import UpdatePlace from "./places/pages/UpdatePlace";
+import Auth from "./user/pages/Auth";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import { AuthContext } from "./shared/context/auth-context";
 
-import React from "react";
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-function App() {
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Users />} />
-        <Route path="places">
-          <Route path="new" element={<NewPlace />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+    >
+      <BrowserRouter>
+        <MainNavigation />
+        <main>
+          <Routes>
+            <Route path="/" element={<Users />} />
+            <Route path="/:userId/places" element={<UserPlaces />} />
+            {isLoggedIn && (
+              <Route path="places">
+                <Route path="new" element={<NewPlace />} />
+                <Route path=":placeId" element={<UpdatePlace />} />
+              </Route>
+            )}
+            {!isLoggedIn && <Route path="/auth" element={<Auth />} />}
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
+    </AuthContext.Provider>
   );
-}
+};
 
 export default App;
