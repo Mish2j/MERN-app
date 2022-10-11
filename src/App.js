7a3d3595aef08c754107fd9ea992b1ed8f102126
Mish2j <mishelghukasyan@gmail.com>
2022-10-11 15:48:46 -0700

    added code splitting

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+import React, { Suspense } from "react";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 import { AuthContext } from "./shared/context/auth-context";
 import useAuth from "./shared/hooks/auth-hook";
 
-import Users from "./user/pages/Users";
-import NewPlace from "./places/pages/NewPlace";
-import UserPlaces from "./places/pages/UserPlaces";
-import UpdatePlace from "./places/pages/UpdatePlace";
-import Auth from "./user/pages/Auth";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import LoadingSpinner from "./shared/components/UIElements/LoadingSpinner";
+
+const Users = React.lazy(() => import("./user/pages/Users"));
+const UserPlaces = React.lazy(() => import("./places/pages/UserPlaces"));
+const NewPlace = React.lazy(() => import("./places/pages/NewPlace"));
+const UpdatePlace = React.lazy(() => import("./places/pages/UpdatePlace"));
+const Auth = React.lazy(() => import("./user/pages/Auth"));
 
 const App = () => {
   const { token, login, logout, userId } = useAuth();
@@ -26,18 +29,26 @@ const App = () => {
       <BrowserRouter>
         <MainNavigation />
         <main>
-          <Routes>
-            <Route path="/" element={<Users />} />
-            <Route path="/:userId/places" element={<UserPlaces />} />
-            {token && (
-              <Route path="places">
-                <Route path="new" element={<NewPlace />} />
-                <Route path=":placeId" element={<UpdatePlace />} />
-              </Route>
-            )}
-            {!token && <Route path="/auth" element={<Auth />} />}
-            <Route path="*" element={<Navigate to="/" />} />
-          </Routes>
+          <Suspense
+            fallback={
+              <div className="center">
+                <LoadingSpinner />
+              </div>
+            }
+          >
+            <Routes>
+              <Route path="/" element={<Users />} />
+              <Route path="/:userId/places" element={<UserPlaces />} />
+              {token && (
+                <Route path="places">
+                  <Route path="new" element={<NewPlace />} />
+                  <Route path=":placeId" element={<UpdatePlace />} />
+                </Route>
+              )}
+              {!token && <Route path="/auth" element={<Auth />} />}
+              <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+          </Suspense>
         </main>
       </BrowserRouter>
     </AuthContext.Provider>
